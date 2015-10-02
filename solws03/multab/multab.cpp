dcbe0108b10e8085e48a47d09653b857d1067d3c
Peter Sommerlad <peter.sommerlad@hsr.ch>
2015-10-02 16:03:50 +0200

    nicer multiplication table code

--- solws03/multab/multab.cpp
+++ solws03/multab/multab.cpp
@@ -3,15 +3,21 @@
 #include <iomanip>
 #include <iterator>
 #include <algorithm>
+
+std::string to_string_width5(unsigned num) {
+	std::ostringstream os { };
+	os << std::setw(5) << num;
+	return os.str();
+}
+
 int main(){
 	std::vector<unsigned> v(20);
 	iota(begin(v),end(v),1);
-	std::ostream_iterator<int> out{std::cout,""};
+	std::ostream_iterator<std::string> out{std::cout,""};
 	for_each(cbegin(v),cend(v),[=](unsigned row){
 		transform(cbegin(v),cend(v),out,
 				[=](unsigned col){
-					std::cout.width(4); // nasty side effect on global...
-					return col*row;
+					return to_string_width5(col* row);
 				});
 		std::cout << '\n';
 	});
