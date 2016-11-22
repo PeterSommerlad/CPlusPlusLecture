e13adef9b983892ffd11c3e4ff362f0504fe89a9
Thomas Corbat <tcorbat@hsr.ch>
2016-11-22 21:29:53 +0100

    add woccurrence project

---
+++ exws10/woccurrence/src/woccurrence.cpp
@@ -0,0 +1,8 @@
+#include "woccurrence.h"
+
+#include <istream>
+#include <ostream>
+
+void woccurence(std::istream & in, std::ostream & out) {
+	//TODO: Implement your woccurence functionality here
+}
