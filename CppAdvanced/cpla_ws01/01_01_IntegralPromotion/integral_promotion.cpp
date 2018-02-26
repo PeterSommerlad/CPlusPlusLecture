dc50dc22fde2977cebee55c562d562dc8ae5f161
Thomas Corbat <tcorbat@hsr.ch>
2018-02-26 08:58:46 +0100

    synced integral promotion with lecture

--- CppAdvanced/cpla_ws01/01_01_IntegralPromotion/integral_promotion.cpp
+++ CppAdvanced/cpla_ws01/01_01_IntegralPromotion/integral_promotion.cpp
@@ -24,3 +24,29 @@ int main() {
 		std::cout << "should be out of bounds";
 	}
 }
+#include <iostream>
+
+
+int main() {
+	int intValue1 = 15;
+	int intValue2 = 24;
+	auto intIntSum = intValue1 + intValue2;
+
+	long longValue1 = 11l;
+	auto longIntSum = longValue1 + intValue1;
+
+	double doubleValue = 128.0;
+	auto doubleIntSum = doubleValue + intValue1;
+
+	unsigned unsignedValue = 99u;
+	auto unsignedIntSum = 0u;//unsignedValue + intValue1;
+
+	int zeroIndex = 0;
+	for (unsigned size = 5; size <= 10; size--) {
+		if (zeroIndex <= size - 1) {
+		  std::cout << "access with 0 is ok for size " << size << '\n';
+		} else {
+		  std::cout << "access with 0 is not ok for size " << size << '\n';
+		}
+	}
+}
